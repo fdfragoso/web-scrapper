4c53ec72d4b791674526731dcaff08b798ed7fc1
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-08-12 19:24:38 +0200

    Update main_spec.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 RSpec.describe UniversityPrograms do
   obj_uni = UniversityPrograms.new('games')
-
+  
   describe '#parsing' do
     it 'returns the university programs' do
       expect(obj_uni.parsing).not_to be_empty
