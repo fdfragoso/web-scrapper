9b065f26646f069120995e9efd1b994e49fff25b
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-08-12 18:21:40 +0200

    Add rspec and gitignore

---
+++ spec/main_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/scrapper.rb'
+
+describe UniversityPrograms do
+  obj_uni = UniversityPrograms.new
+
+  describe '#parsing' do
+    it 'returns the university programs' do
+      expect(obj_uni.parsing).not_to be_empty
+    end
+
+    it 'returns the university programs' do
+      expect(obj_uni.parsing).not_to be_nil
+    end
+  end
+end
